1c0bc913b2b86411a3a060fba3a845f715b17172
Badal Dadwani <76866736+Badal023@users.noreply.github.com>
2025-10-24 17:21:19 +0530

    Update B&W.py
    Added code for conversion of golden PCBs as well

--- B&W.py
+++ B&W.py
@@ -6,6 +6,18 @@ errors = ["missing_hole", "mouse_bite", "open_circuit", "short", "spur", "spurio
 #Creating folders (if not already there)
 for error_type in range(6):
     os.makedirs(f"Output_data/BW_images/{errors[error_type]}", exist_ok=True)
+os.makedirs("Output_data/BW_images/PCB_USED", exist_ok=True)
+
+#converting the golden pcbs to B&W
+for pcb_type in range(1,13):
+    try:
+        img = cv2.imread(f"PCB_DATASET/PCB_USED/{pcb_type:02d}.jpg")
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        cv2.imwrite(f"Output_data/BW_images/PCB_USED/{pcb_type:02d}.jpg", img)
+
+    except cv2.error:
+        print("Changing PCB")
+        continue
 
 #Converting all images from color to B&W and saving them
 flag = False
